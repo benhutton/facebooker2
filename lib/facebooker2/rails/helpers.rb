7b365458630f09e6db2645acdd2984158a4a286a
Ryan Walker <wwwguru@gmail.com>
2010-08-17 20:35:19 -0400

    document and minor refactor

--- lib/facebooker2/rails/helpers.rb
+++ lib/facebooker2/rails/helpers.rb
@@ -5,29 +5,34 @@ module Facebooker2
       include Javascript
       include RequestForms
       include User
-      
-       def fb_stringify_vals(hash)
+
+      # converts all values of hash to strings
+      def fb_stringify_vals(hash)
         result={}
         hash.each do |key,value|
           result[key]=value.to_s
         end
         result
-       end
-        def fb_transform_keys(options,transformation_hash)
-          new_hash = {}
-          options.each do |key,value|
-            new_key = transformation_hash[key]||key
-            new_hash[new_key]=value
-          end
-          new_hash
+      end
+
+      # transforms keys based on supplied transformation hash
+      def fb_transform_keys(options,transformation_hash)
+        new_hash = {}
+        options.each do |key,value|
+          new_key = transformation_hash[key]||key
+          new_hash[new_key]=value
         end
-        FB_ALWAYS_VALID_OPTION_KEYS = [:class, :style]
-      
+        new_hash
+      end
+
+      FB_ALWAYS_VALID_OPTION_KEYS = [:class, :style]
+
+      # asserts that keys of a hash are included in a supplied list of valid keys
       def fb_assert_valid_keys(options,*valid_keys)
         unknown_keys = options.keys - [valid_keys + FB_ALWAYS_VALID_OPTION_KEYS].flatten
         raise(ArgumentError, "Unknown key(s): #{unknown_keys.join(", ")}") unless unknown_keys.empty?
-      end    
-      
+      end
+
     end
   end
 end
--- lib/facebooker2/rails/helpers/user.rb
+++ lib/facebooker2/rails/helpers/user.rb
@@ -2,9 +2,26 @@ module Facebooker2
   module Rails
     module Helpers
       module User
+        FB_NAME_OPTION_KEYS_TO_TRANSFORM = {:first_name_only => :firstnameonly,
+                                            :last_name_only => :lastnameonly,
+                                            :show_network => :shownetwork,
+                                            :use_you => :useyou,
+                                            :if_cant_see => :ifcantsee,
+                                            :subject_id => :subjectid}
+
+        FB_NAME_VALID_OPTION_KEYS = [:firstnameonly, :linked, :lastnameonly, :possessive, :reflexive,
+                                     :shownetwork, :useyou, :ifcantsee, :capitalize, :subjectid]
+
+        FB_PROFILE_PIC_OPTION_KEYS_TO_TRANSFORM = {:facebook_logo => 'facebook-logo'}
+
+        FB_PROFILE_PIC_VALID_OPTION_KEYS = [:size, :linked, 'facebook-logo', :width, :height]
+
+        VALID_FB_PROFILE_PIC_SIZES = [:thumb, :small, :normal, :square]
+
+        ###
         # Render an fb:name tag for the given user
-        # This renders the name of the user specified.  You can use this tag as both subject and object of 
-        # a sentence.  <em> See </em> http://wiki.developers.facebook.com/index.php/Fb:name for full description.  
+        # This renders the name of the user specified.  You can use this tag as both subject and object of
+        # a sentence.  <em> See </em> http://wiki.developers.facebook.com/index.php/Fb:name for full description.
         # Use this tag on FBML pages instead of retrieving the user's info and rendering the name explicitly.
         #
         def fb_name(user, options={})
@@ -14,16 +31,10 @@ module Facebooker2
           content_tag("fb:name",nil, fb_stringify_vals(options))
         end
 
-        FB_NAME_OPTION_KEYS_TO_TRANSFORM = {:first_name_only => :firstnameonly, 
-                                            :last_name_only => :lastnameonly,
-                                            :show_network => :shownetwork,
-                                            :use_you => :useyou,
-                                            :if_cant_see => :ifcantsee,
-                                            :subject_id => :subjectid}
-        FB_NAME_VALID_OPTION_KEYS = [:firstnameonly, :linked, :lastnameonly, :possessive, :reflexive, 
-                                     :shownetwork, :useyou, :ifcantsee, :capitalize, :subjectid]
-        
-        
+        ###
+        # Render an fb:profile-pic tag for the given user
+        # <em> See </em> http://developers.facebook.com/docs/reference/fbml/profile-pic for full description.
+        #
         def fb_profile_pic(user, options={})
           options = options.dup
           validate_fb_profile_pic_size(options)
@@ -33,16 +44,16 @@ module Facebooker2
           content_tag("fb:profile-pic", nil,fb_stringify_vals(options))
         end
 
-        FB_PROFILE_PIC_OPTION_KEYS_TO_TRANSFORM = {:facebook_logo => 'facebook-logo'}
-        FB_PROFILE_PIC_VALID_OPTION_KEYS = [:size, :linked, 'facebook-logo', :width, :height]
-        VALID_FB_PROFILE_PIC_SIZES = [:thumb, :small, :normal, :square]
+        private
+        ###
+        # ensures that requested profile pic size is valid
+        #
         def validate_fb_profile_pic_size(options)
           if options.has_key?(:size) && !VALID_FB_PROFILE_PIC_SIZES.include?(options[:size].to_sym)
             raise(ArgumentError, "Unknown value for size: #{options[:size]}")
           end
         end
 
-       
       end
     end
   end
